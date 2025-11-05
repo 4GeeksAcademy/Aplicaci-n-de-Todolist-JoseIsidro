87cc10a557d9b00600a475db9ddb1732d8fbb340
José Isidro <isidro107109@gmail.com>
2025-11-05 21:25:54 +0000

    estructura sin desafio completo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [list, setList] = useState([]);
+	const [tarea, setTarea] = useState('');
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	const nuevaTarea = (e) => {
+		if (e.key === "Enter" && tarea.trim() !== "") {
+			setList([...list, tarea]);
+			setTarea('');
+		}
+	}
+	function eliminar(idEliminar) {
+		let filtroDelist = list.filter((_, index) => index !== idEliminar);
+		setList(filtroDelist);
+	}
+
+	return (
+		<div className="container w-50 p-3">
+			<h1 className="text-center">Todos</h1>
+			<ul className="list-group">
+				<li className="list-group-item px-5">
+					<input
+						className="border-0 my-1 fs-3 text-secondary"
+						type="text"
+						onChange={(e) => setTarea(e.target.value)}
+						onKeyDown={nuevaTarea}
+						placeholder="What need to be done?" />
+				</li>
+
+
+				{list.map((item, id) => (
+					<li className="list-group-item d-flex justify-content-between list-group-item-action px-5" key={id}>
+						<p className="fs-3 text-secondary my-auto">{item}</p>
+						<button type="button" class="btn-close my-auto" aria-label="Close" onClick={() => eliminar(id)}></button>
+					</li>
+				))}
+
+				<li className="list-group-item">
+					<small class="text-body-secondary">{list.length} item left</small>
+					
+				</li>
+			</ul>
+		</div >
 	);
 };
 
